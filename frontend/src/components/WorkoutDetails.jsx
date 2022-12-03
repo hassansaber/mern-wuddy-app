1cba5d8f61f9eedc35bc358bdaf03f6d85198c19
hassansaber <hassansaberkarin@gmail.com>
2022-12-03 12:47:41 +0330

    fetching data

---
+++ frontend/src/components/WorkoutDetails.jsx
@@ -0,0 +1,20 @@
+const WorkoutDetails = ({ workout }) => {
+  const { title, load, reps, createdAt } = workout;
+
+  return (
+    <div className="workout-details">
+      <h4>{title}</h4>
+      <p>
+        <strong>Load (kg) : </strong>
+        {load}
+      </p>
+      <p>
+        <strong>Reps : </strong>
+        {reps}
+      </p>
+      <p>{createdAt}</p>
+    </div>
+  );
+};
+
+export default WorkoutDetails;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails";
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  // run when component rendered , "[]" runs once
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+      if (response.ok) setWorkouts(json);
+    };
+
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {/* cycle through the workouts ,only when we have workout*/}
+        {workouts &&
+          // (...) in arrow means return(...)
+          workouts.map((workout) => <WorkoutDetails key={workout._id} workout={workout} />)}
+      </div>
     </div>
   );
 };
