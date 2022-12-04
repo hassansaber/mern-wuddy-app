0ef5d19531b6bead4d9113d52400cf48bf1359a0
hassansaber <hassansaberkarin@gmail.com>
2022-12-04 10:47:22 +0330

    deleting data

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,5 +1,19 @@
+const { useWorkoutContext } = require("../hooks/useWorkoutContext");
+
 const WorkoutDetails = ({ workout }) => {
   const { title, load, reps, createdAt } = workout;
+  const { dispatch } = useWorkoutContext();
+
+  const handleClick = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
 
   return (
     <div className="workout-details">
@@ -13,6 +27,7 @@ const WorkoutDetails = ({ workout }) => {
         {reps}
       </p>
       <p>{createdAt}</p>
+      <span onClick={handleClick}>Delete</span>
     </div>
   );
 };
