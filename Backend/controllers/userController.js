b96d7162a15ce90d7176d7eae5703fde72d73367
hassansaber <hassansaberkarin@gmail.com>
2022-12-28 21:34:29 +0330

    auth - Email & Password Validation

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -9,7 +9,19 @@ const loginUser = async (req, res) => {
 
 // signup user
 const signupUser = async (req, res) => {
-  res.json({ message: 'signup user' })
+
+  // get email and password from req
+  const { email, password } = req.body
+
+  try {
+    //sign up using static
+    const user = await User.signup(email, password)
+
+    // must use {} inside json
+    res.status(201).json({ email, user }) // user => whole new document
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
 }
 
 
--- Backend/model/userModel.js
+++ Backend/model/userModel.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
 
+// schema
 const Schema = mongoose.Schema
-
 const userSchema = new Schema({
   email: {
     type: String,
@@ -15,4 +17,37 @@ const userSchema = new Schema({
   }
 })
 
+// static signup method
+// must be regular func because of "this" keyword
+userSchema.statics.signup = async function (email, password) {
+
+
+  // validation
+  if (!email || !password) throw Error('All fields must be filled')
+  if (!validator.isEmail(email)) throw Error('Email is not valid')
+  if (!validator.isStrongPassword(password)) throw Error('Password is not strong enough')
+
+
+  // check email existence 
+  // this is actually User model 
+  const exists = await this.findOne({ email })
+  if (exists) throw Error("Email already in use")
+
+  // salt is sth will added to user password => dgnfdjghkcg ===> userpassworddgnfdjghkcg
+  // salt prevent hackers to password matching
+
+  // password hashing
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
+
+  // store email &  password in new document
+  const user = await this.create({
+    email,
+    password: hash
+  })
+
+  return user
+}
+
+// model
 module.exports = mongoose.model('User', userSchema)
