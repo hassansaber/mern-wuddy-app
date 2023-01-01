0b50f0e98885fd91cec0eb4349676d378cde9b8c
hassansaber <hassansaberkarin@gmail.com>
2023-01-02 00:38:38 +0330

    auth - Setting the Initial Auth Status

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 export const AuthContext = createContext();
 
@@ -23,6 +23,19 @@ export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
   });
+
+  // when page refresh  or when app starts
+  useEffect(() => {
+    // checking storage for token
+    // JWT in local storage is a json-string
+    const user = JSON.parse(localStorage.getItem("user"));
+    // user now is json-object
+
+    // update context when storage have user
+    if (user) {
+      dispatch({ type: "LOGIN", payload: user });
+    }
+  }, []); // [] means hook runs once
 
   console.log("Auth Context :", state);
 
